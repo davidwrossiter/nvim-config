4f4fae71e0501fd177afe46fce85e482f98d5f82
davidwrossiter <davewrossiter@gmail.com>
2024-11-26 19:11:38 +0000

    minor updates

--- init.lua
+++ init.lua
@@ -612,6 +612,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        html = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
